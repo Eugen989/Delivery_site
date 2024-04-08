1051116f7085ea34c6706e5147c80b960606a37d
RussianFoxBryansk <138440961+RussianFoxBryansk@users.noreply.github.com>
2024-04-08 13:10:17 +0300

    mongodb _1

--- delivery-site/src/database/bd.js
+++ delivery-site/src/database/bd.js
@@ -1,12 +1,39 @@
+// import { MongoClient } from 'mongodb';
+
+// const client = new MongoClient('mongodb+srv://russianfox36:ExqfAv0YyFiJ7P4t@cluster0.tcjngn3.mongodb.net/delivery_site?retryWrites=true&w=majority&appName=Cluster0');
+
+// const start = async () => {
+//   try {
+//     await client.connect();
+//     console.log('Соединение установлено');
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// start();
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient('mongodb+srv://russianfox36:ExqfAv0YyFiJ7P4t@cluster0.tcjngn3.mongodb.net/delivery_site?retryWrites=true&w=majority&appName=Cluster0')
+const uri = 'mongodb+srv://russianfox36:ExqfAv0YyFiJ7P4t@cluster0.tcjngn3.mongodb.net/delivery_site?retryWrites=true&w=majority&appName=Cluster0'; // URL-адрес для подключения к MongoDB (по умолчанию localhost:27017)
+const client = new MongoClient(uri);
 
-const start = async () =>{
-    try{
-        await client.connect()
-        console.log('Соединение установлено')
-    } catch(e){
-        console.log(e)
-    }
+let db;
+
+async function connect() {
+  try {
+    await client.connect();
+    db = client.db('my_database'); // Замените 'my_database' на имя вашей базы данных
+    console.log('Connected to MongoDB.');
+  } catch (err) {
+    console.error('Connection to MongoDB failed:', err);
+  }
 }
+
+function getDb() {
+  if (!db) {
+    throw new Error('Database not connected.');
+  }
+  return db;
+}
+
+export { connect, getDb };
