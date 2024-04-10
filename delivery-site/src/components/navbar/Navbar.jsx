a3aa61c56aebaf77996fa79a3a211a4d7292518f
Kostess <qaz1234567890838@gmail.com>
2024-04-10 16:22:38 +0300

    слегка переработан navbar

--- delivery-site/src/components/navbar/Navbar.jsx
+++ delivery-site/src/components/navbar/Navbar.jsx
@@ -1,37 +1,41 @@
-import "./navbar.module.css"
-
-// Маршруты
-// /
-// /products
-// /login
+import "./navbar.css";
 
 function Navbar() {
     return (
-        <div className="wrapper">
+        <div className="navbar">
             <header className="header">
                 <div className="centring">
                     <div className="header-flex">
                         <a href="/" className="logo">
                             logo
                         </a>
-                        <a href="/products" className="catalog-link">
-                            Каталог
-                        </a>
-                        <form action="#" className="form-search">
-                            <input type="search" placeholder="Поиск..."/>
-                        </form>
-                        <a href="/basket" className="basket-link">
-                            <img src="images/basket.svg" alt="basket"/>
-                            <span className="indicator-buy text-1">3</span>
-                        </a>
-                        <div className="profile-link">
-                            <a href="/login" className="no-profile no-profile--active">
-                                <img src="images/no-profile.svg" alt=""/>
-                                Войти
+                        <div className="menu text-3">
+                            <li className="menu-item">
+                                <a href="/" className="menu-link">Гланая</a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="/products" className="menu-link">
+                                    Каталог
+                                </a>
+                            </li>
+                            <li className="menu-item">
+                                <a href="" className="menu-link">О нас</a>
+                            </li>
+                        </div>
+                        <div className="right-navbar">
+                            <a href="/basket" className="basket-link">
+                                <img src="images/basket.svg" alt="basket"/>
+                                <span className="indicator-buy text-1">3</span>
                             </a>
-                            <a href="/profile" className="yes-profile">
-                                <img src="images/avatar-profile.png" alt=""/>
-                            </a>
+                            <div className="profile-link">
+                                <a href="/login" className="no-profile no-profile--active">
+                                    <img src="images/no-profile.svg" alt=""/>
+                                    Войти
+                                </a>
+                                <a href="/profile" className="yes-profile">
+                                    <img src="images/avatar-profile.png" alt=""/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
