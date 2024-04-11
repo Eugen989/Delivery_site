a9e0342a3b5ade88231daf9fb1ad1d37faefc2fc
Eugen <kruglikeugen@gmail.com>
2024-04-11 16:51:24 +0300

    The first unification of the front and the database

--- react-client-server/client/src/index.js
+++ react-client-server/client/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import {store} from "./reducers";
-import {Provider} from "react-redux";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById("root"));
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const devMode = process.env.NODE_ENV = "development";
+if (devMode && module && module.hot) {
+  module.hot.accept();
+}
+
+//reportWebVitals();
