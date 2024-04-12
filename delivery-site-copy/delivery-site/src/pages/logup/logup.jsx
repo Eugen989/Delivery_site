2c2e5c56eb53a8e1db87bbe2fe0fb19c89f558c0
Eugen <kruglikeugen@gmail.com>
2024-04-12 20:22:24 +0300

    Processing registration and authorization 3

--- delivery-site-copy/delivery-site/src/pages/logup/logup.jsx
+++ delivery-site-copy/delivery-site/src/pages/logup/logup.jsx
@@ -1,36 +1,11 @@
 import classes from "./logup.module.css";
-import { MyInput, InputSelect } from "../../components/UI/input/MyInput.jsx";
+import { MyInput, InputSelect, RadioBtn } from "../../components/UI/input/MyInput.jsx";
 import { MyButton1 } from "../../components/UI/button/MyButton.jsx";
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
-
-const RadioBtn = ({ id, name, value }) => {
-    return (
-        <input type="radio" id={id} name={name} value={value} required/>
-    );
-};
+import React, {useEffect, useState} from "react";
 
 const Logup = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-    const [dataAuth, setDataAuth] = useState([]);
-
-    const [formData, setFormData] = useState({
-        name: '',
-        secondName: '',
-        email: '',
-        password: '',
-        userType: ''
-      });
-
-    const [valueSelectRegistering, setVal] = useState('buyer')
-    const handlerChange = e => {
-        setVal(e.target.value)
-    }
-
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -39,7 +14,7 @@ const Logup = () => {
                 name: formData.name,
                 email: formData.email,
                 password: formData.password,
-                userType: valueSelectRegistering
+                userType: val
             });
             console.log(response.data);
             // Дополнительные действия после успешного запроса
@@ -51,6 +26,7 @@ const Logup = () => {
 
     const handleAccept = async (e) => {
         e.preventDefault();
+        setFormData({ ...formData, email: formData.email, password: formData.password });
         try {
             const response = await axios.get('http://localhost:5000/api/auth/signIn', {
                 params: {
@@ -70,69 +46,74 @@ const Logup = () => {
         setIsRegistering(prevState => !prevState);
     };
 
-    // useEffect(() => {
-    //     console.log(dataAuth);
-    //     if(dataAuth) {
-    //         axios.post("http://localhost:5000/api/auth/signUp")
-    //         .then((response) => {
-    //         setDataAuth(response.data.values);
-    //         console.log(response.data);
-    //         });
-    //     }
-    // }, [])
+    const [valueSelectRegistering, setVal] = useState('buyer')
+    const handlerChange = e => {
+        setVal(e.target.value)
+    }
 
     return (
         <div className="logup">
-            {!isRegistering ?
+            {isRegistering ?
                 <div className="centring">
                 <div className={`${classes.loginWrapper} mt-1`}>
-                    <h2 className="title-3">
+                    <h2 className={`${classes.titleForm} title-3`}>
                         Регистрация
                     </h2>
                     <form action="" method="POST" className={classes.formBlock} onSubmit={handleSubmit}>
-                        <MyInput type="name" placeholder="Имя пользователя" name="name" value={formData.name} onChange={handleChange} required/>
-                        <MyInput type="email" placeholder="Электронная почта" name="email" value={formData.email} onChange={handleChange} required/>
-                        <MyInput type="password" placeholder="Пароль" name="password" value={formData.password} onChange={handleChange} required/>
+                        <MyInput type="email" placeholder="Электронная почта" required/>
+                        <MyInput placeholder="Имя пользователя" required/>
+                        <MyInput type="password" placeholder="Пароль" required/>
                         <MyInput type="password" placeholder="Повторить пароль" required/>
                         <div className={classes.choice}>
-                            <label htmlFor="shippingMethod">Выберите:</label>
-                            <InputSelect id="shippingMethod" name="shippingMethod" required onChange={handlerChange}>
-                                <option value="buyer">Я покупатель</option>
-                                <option value="salesman">Я продавец</option>
-                            </InputSelect>
+                            <RadioBtn
+                                text="Я покупатель"
+                                name="choice"
+                                value="buyer"
+                                onChange={handlerChange}
+                                checked={valueSelectRegistering === 'buyer'}
+                            />
+                            <RadioBtn
+                                text="Я продавец"
+                                name="choice"
+                                value="seller"
+                                onChange={handlerChange}
+                                checked={valueSelectRegistering === 'seller'}
+                            />
                         </div>
+
                         <div className={`${classes.btnForm} ${classes.btnFormIn}`}>
                             <MyButton1 type="submit">
                                 Отправить
                             </MyButton1>
                         </div>
                     </form>
-                    <a onClick={toggleForm} className={classes.linkLogup}>У меня уже есть аккаунт</a>
+                    <a onClick={toggleForm} className={classes.linkLoginOut}>У меня уже есть аккаунт</a>
                 </div>
-            </div>
-            
-        :
-            <div className="centring">
-                <div className={`${classes.loginWrapper} mt-1`}>
-                    <h2 className="title-3">
-                        Авторизация
-                    </h2>
-                    <div className={classes.formBlock}>
-                        <form onSubmit={handleAccept}>
-                            <div className="inputBlock">
-                                <MyInput type="email" placeholder="Электронная почта" name="email" value={formData.email} onChange={handleChange} required/>
-                                <MyInput type="password" placeholder="Пароль" name="password" value={formData.password} onChange={handleChange} required/>
+                </div>
+                :
+                <div className="centring">
+                    <div className={`${classes.loginWrapper} mt-1`}>
+                        <h2 className={`${classes.titleForm} title-3`}>
+                            Авторизация
+                        </h2>
+                        <div className={classes.formBlock}>
+                            <form onSubmit={handleAccept}>
+                                <div className="inputBlock">
+                                    <MyInput value={email} onChange={(e) => setEmail(e.target.value)} type="email"
+                                             placeholder="Электронная почта" required/>
+                                <MyInput value={pass} onChange={(e) => setPass(e.target.value)} type="password"
+                                         placeholder="Пароль" required/>
                             </div>
                             <div className={classes.btnForm}>
-                                <a onClick={toggleForm} className={classes.linkLogin}>
-                                    У меня нет аккаунта
-                                </a>
                                 <MyButton1 type="submit">
                                     Отправить
                                 </MyButton1>
-                            </div>
-                        </form>
-                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    <a onClick={toggleForm} className={classes.linkLoginOut}>
+                        У меня нет аккаунта
+                    </a>
                 </div>
             </div>
             }
