8c6a644119f901dc2a710bfea401afef6ceb84ec
RussianFoxBryansk <138440961+RussianFoxBryansk@users.noreply.github.com>
2024-04-13 04:31:47 +0300

    Сортировка и фильтрация

--- delivery-site-copy/delivery-site/src/pages/products/Products.jsx
+++ delivery-site-copy/delivery-site/src/pages/products/Products.jsx
@@ -1,20 +1,69 @@
 import React, { useState } from "react";
 import classes from "./products.module.css";
 import CardItem from "../../components/cardItem/CardItem";
-import {InputFilter} from "../../components/UI/input/MyInput.jsx";
-import {MyButton2} from "../../components/UI/button/MyButton.jsx";
-
-// localStorage.clear();
-// localStorage.setItem('UserData', JSON.stringify({userId: 1, userName: '', userType: ''}));
-// console.log(JSON.parse(localStorage.getItem('UserData')))
+import { InputFilter } from "../../components/UI/input/MyInput.jsx";
+import { MyButton2 } from "../../components/UI/button/MyButton.jsx";
 
 const Products = () => {
     const [cards, setCards] = useState([
-        { id: 1, title: "Картошечка",warehouse:"Минск",massa:54,size:34,body: "Очень очень вкусно",price:777,price_now:888 },
-        { id: 2, title: "Драники",warehouse:"Брянск",massa:0.750,size:17,body: "Сделано из картошки",price:799,price_now:988},
-        { id: 3, title: "Блины",warehouse:"Москва",massa:0.250,size:54,body: "С добавлением картошки",price:997,price_now:898 },
+        { id: 1, title: "Картошечка", warehouse: "Минск", massa: 54, size: 34, body: "Очень очень вкусно", price: 777, price_now: 888 },
+        { id: 2, title: "Драники", warehouse: "Брянск", massa: 0.750, size: 17, body: "Сделано из картошки", price: 799, price_now: 988 },
+        { id: 3, title: "Блины", warehouse: "Москва", massa: 0.250, size: 54, body: "С добавлением картошки", price: 997, price_now: 898 },
     ]);
 
+    const [sortBy, setSortBy] = useState('');
+    // State to hold the filter values
+    const [filterValues, setFilterValues] = useState({
+        priceMin: "",
+        priceMax: "",
+        sizeMin: "",
+        sizeMax: "",
+        massaMin: "",
+        massaMax: ""
+    });
+
+    // Function to handle changes in filter inputs
+    const handleFilterChange = (e) => {
+        const { name, value } = e.target;
+        setFilterValues({
+            ...filterValues,
+            [name]: value
+        });
+    };
+
+    // Function to apply filters
+    const applyFilters = () => {
+        const filteredCards = cards.filter(card => {
+            return (
+                (filterValues.priceMin === "" || card.price_now >= parseInt(filterValues.priceMin)) &&
+                (filterValues.priceMax === "" || card.price_now <= parseInt(filterValues.priceMax)) &&
+                (filterValues.sizeMin === "" || card.size >= parseInt(filterValues.sizeMin)) &&
+                (filterValues.sizeMax === "" || card.size <= parseInt(filterValues.sizeMax)) &&
+                (filterValues.massaMin === "" || card.massa >= parseFloat(filterValues.massaMin)) &&
+                (filterValues.massaMax === "" || card.massa <= parseFloat(filterValues.massaMax))
+            );
+        });
+
+        setCards(filteredCards);
+    };
+
+    const sortedItems = [...cards].sort((a, b) => {
+        if (sortBy === 'price_now') {
+            return a.price_now - b.price_now;
+        } else if (sortBy === 'massa') {
+            return a.massa - b.massa;
+        } else if (sortBy === 'size') {
+            if (a.size < b.size) return -1;
+            if (a.size > b.size) return 1;
+            return 0;
+        } else {
+            return 0;
+        }
+    });
+
+    const handleSortChange = (e) => {
+        setSortBy(e.target.value);
+    }
     return (
         <div className="products mt-1">
 
@@ -26,56 +75,80 @@ const Products = () => {
                         </h3>
                         <div className={classes.filterWrapper}>
                             <form action="">
-                                <div className={classes.filterItem}>
-                                    <p className={`${classes.titleFilter} text-3`}>
-                                        Цена
-                                    </p>
-                                    <div className={classes.filterBlock}>
-                                        <InputFilter placeholder="От"/>
-                                        <InputFilter placeholder="До"/>
-                                    </div>
+                            <div className={classes.filterItem}>
+                                <p className={`${classes.titleFilter} text-3`}>
+                                    Цена
+                                </p>
+                                <div className={classes.filterBlock}>
+                                    <InputFilter
+                                        name="priceMin"
+                                        value={filterValues.priceMin}
+                                        onChange={handleFilterChange}
+                                        placeholder="От"
+                                    />
+                                    <InputFilter
+                                        name="priceMax"
+                                        value={filterValues.priceMax}
+                                        onChange={handleFilterChange}
+                                        placeholder="До"
+                                    />
                                 </div>
-                                <div className={classes.filterItem}>
-                                    <p className={`${classes.titleFilter} text-3`}>
-                                        Размер
-                                    </p>
-                                    <div className={classes.filterBlock}>
-                                        <InputFilter placeholder="От"/>
-                                        <InputFilter placeholder="До"/>
-                                    </div>
+                            </div>
+                            <div className={classes.filterItem}>
+                                <p className={`${classes.titleFilter} text-3`}>
+                                    Масса
+                                </p>
+                                <div className={classes.filterBlock}>
+                                    <InputFilter
+                                        name="massaMin"
+                                        value={filterValues.massaMin}
+                                        onChange={handleFilterChange}
+                                        placeholder="От"
+                                    />
+                                    <InputFilter
+                                        name="massaMax"
+                                        value={filterValues.massaMax}
+                                        onChange={handleFilterChange}
+                                        placeholder="До"
+                                    />
                                 </div>
-                                <div className={classes.filterItem}>
-                                    <p className={`${classes.titleFilter} text-3`}>
-                                        Масса
-                                    </p>
-                                    <div className={classes.filterBlock}>
-                                        <InputFilter placeholder="От"/>
-                                        <InputFilter placeholder="До"/>
-                                    </div>
+                            </div>
+                            <div className={classes.filterItem}>
+                                <p className={`${classes.titleFilter} text-3`}>
+                                    Размер
+                                </p>
+                                <div className={classes.filterBlock}>
+                                    <InputFilter
+                                        name="sizeMin"
+                                        value={filterValues.sizeMin}
+                                        onChange={handleFilterChange}
+                                        placeholder="От"
+                                    />
+                                    <InputFilter
+                                        name="sizeMax"
+                                        value={filterValues.sizeMax}
+                                        onChange={handleFilterChange}
+                                        placeholder="До"
+                                    />
                                 </div>
+                            </div>
                                 <div className={classes.btnFilter}>
-                                    <MyButton2 type="submit">Применить</MyButton2>
+                                    <MyButton2 type="button" onClick={applyFilters}>
+                                        Применить
+                                    </MyButton2>
                                 </div>
                             </form>
                         </div>
                     </div>
                     <div className="cardGroup">
-                        <select name="" id="">
-                            <option value="">
-                                По названию
-                            </option>
-                            <option value="">
-                                По цене
-                            </option>
-                            <option value="">
-                                По массе
-                            </option>
-                            <option value="">
-                                По размеру
-                            </option>
-                        </select>
+                    <select onChange={handleSortChange}>
+                        <option value="">По названию</option>
+                        <option value="price">По цене</option>
+                        <option value="weight">По массе</option>
+                        <option value="size">По размеру</option>
+                    </select>
                         {cards.length > 0 ? (
-                            cards.map((card) => (
+                            sortedItems.map((card) => (
                                 <div key={card.id}>
                                     <CardItem title={card.title}
                                               warehouse={card.warehouse}
